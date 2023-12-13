69bb193295b58c1e15574690555f19ed3661f0cc
Anna <annasukharieva0@gmail.com>
2023-12-13 22:39:30 +0200

    added some changes to header and navbar

--- lab/src/components/Footer.js
+++ lab/src/components/Footer.js
@@ -1,36 +1,38 @@
-import React from 'react'
-import LogoB from '../media/logob.png'
-import LogoT from '../media/cutedLogo.png'
+import React from "react";
+import LogoB from "../media/logob.png";
+import LogoT from "../media/cutedLogo.png";
 
 function Footer() {
   return (
-    <div className='bg-[#580000]  w-[100%] h-auto'>
-  <div className="flex">
-  <ul className="text-white font-medium text-m pl-24 mt-[50px] mb-[50px]">
-    <li className="pb-4">Про нас</li>
-    <li className="pb-4">Початковий тест</li>
-    <li>Курс</li>
-  </ul>
+    <div className="bg-[#580000] w-[100%] h-auto">
+      <div className="flex">
+        <ul className="text-white font-medium text-m pl-24 mt-[50px] mb-[50px]">
+          <li className="pb-4">Про нас</li>
+          <li className="pb-4">Початковий тест</li>
+          <li>Курс</li>
+        </ul>
 
-  <ul className="flex items-center flex-col justify-center ml-[350px]">
-  <li className="flex">
-    <img className="mr-1 w-16" src={LogoB} alt="LogoB" />
-    <img className="w-[200px] h-auto" src={LogoT} alt="LogoT" />
-  </li>
-  <li className='mt-2 text-white font-medium text-m  '>2024</li>
-</ul>
+        <ul className="flex items-center flex-col justify-center ml-[350px]">
+          <li className="flex">
+            <img className="mr-1 w-16" src={LogoB} alt="LogoB" />
+            <img className="w-[200px] h-auto" src={LogoT} alt="LogoT" />
+          </li>
+          <li className="mt-2 text-white font-medium text-m  ">2024</li>
+        </ul>
 
-<ul className="flex items-center flex-col justify-center ml-[350px]">
-  <li className="flex">
-    <button className=' pt-2 pb-2 pl-12 pr-12 rounded-lg  text-white font-medium text-m   bg-[red]'>Зареєстуватись</button>
-  </li>
-  <li className='mt-2 pl-4 text-white font-medium text-m  '>Увійти в кабінет</li>
-</ul>
-
-</div>
-
+        <ul className="flex items-center flex-col justify-center ml-[350px]">
+          <li className="flex">
+            <button className=" pt-2 pb-2 pl-12 pr-12 rounded-lg  text-white font-medium text-m   bg-[red]">
+              Зареєстуватись
+            </button>
+          </li>
+          <li className="mt-2 pl-4 text-white font-medium text-m  ">
+            Увійти в кабінет
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- lab/src/components/NavbarCourse.js
+++ lab/src/components/NavbarCourse.js
@@ -1,5 +1,5 @@
-import React , {useState} from 'react'
-import RedLogo from '../media/RegLogo.png'
+import React, { useState } from "react";
+import RedLogo from "../media/RegLogo.png";
 import { TiThMenu } from "react-icons/ti";
 import { AiOutlineClose } from "react-icons/ai";
 import { MdAccountCircle } from "react-icons/md";
@@ -15,48 +15,48 @@ function NavbarCourse() {
     setNav(!nav);
   };
 
-
- 
- 
-
   return (
-    <div >
+    <div className="sticky top-0">
       <div>
-        <ul className='flex flex-row border border-b-2'>
-          <li className='w-[18%] p-[1%]'>
-          <img src={RedLogo} alt="Logo" />
+        <ul className="flex flex-row border border-b-2 bg-[#ffffff] ">
+          <li className="w-[18%] p-[1%]">
+            <img src={RedLogo} alt="Logo" />
           </li>
-          <li className='pl-[75%] block pt-[1%]'>
-        <MdAccountCircle color='red' size={40} />
-      </li>
-      <li onClick={handleNav} className='pt-[1%]' >
-        <TiThMenu color='red' size={40} />
-      </li>
+          <li className="pl-[75%] block pt-[1%]">
+            <MdAccountCircle color="red" size={40} />
+          </li>
+          <li onClick={handleNav} className="pt-[1%]">
+            <TiThMenu color="red" size={40} />
+          </li>
         </ul>
 
         {nav && (
-        <div className='fixed left-0 top-0 w-full bg-[#ffffff] ease-in-out duration-500 h-full z-10 '>
-          <div className='pl-[1440px] pt-[25px] md:flex hover:text-red-500'><AiOutlineClose color='black' size={40} onClick={handleNav}/></div>
-          <ul className='pt-32 uppercase flex flex-col items-center h-full w-full'>
-  <li className='p-2 text-center flex items-center text-3xl font-extrabold hover:text-red-500 hover:translate-x-5 transition-transform duration-300'>
-    <BsHouseDoor size={30} className='mr-1'/> Головна
-  </li>
-  <li className='p-3 text-center flex items-center text-3xl font-extrabold mr-20 hover:text-red-500 hover:translate-x-5 transition-transform duration-300'>
-    <RiPresentationLine size={30} className='mr-1'  />Курс
-  </li>
-  <li className='p-3 text-center flex items-center text-3xl font-extrabold mr-4  hover:text-red-500 hover:translate-x-5 transition-transform duration-300'>
-    <RiGroupLine size={30} className='mr-1'  />Про нас
-  </li>
-  <li className='p-3 text-center flex items-center text-3xl font-extrabold mr-20 hover:text-red-500 hover:translate-x-5 transition-transform duration-300'>
-    <GoTasklist size={30} className='mr-1'/>Тест
-  </li>
-</ul>
-        </div>
-      )}
+          <div className="fixed left-0 top-0 w-full bg-[#ffffff] ease-in-out duration-500 h-full z-10 ">
+            <div className="pl-[1440px] pt-[25px] md:flex hover:text-red-500">
+              <AiOutlineClose color="black" size={40} onClick={handleNav} />
+            </div>
+            <ul className="pt-32 uppercase flex flex-col items-center h-full w-full">
+              <li className="p-2 text-center flex items-center text-3xl font-extrabold hover:text-red-500 hover:translate-x-5 transition-transform duration-300">
+                <BsHouseDoor size={30} className="mr-1" /> Головна
+              </li>
+              <li className="p-3 text-center flex items-center text-3xl font-extrabold mr-20 hover:text-red-500 hover:translate-x-5 transition-transform duration-300">
+                <RiPresentationLine size={30} className="mr-1" />
+                Курс
+              </li>
+              <li className="p-3 text-center flex items-center text-3xl font-extrabold mr-4  hover:text-red-500 hover:translate-x-5 transition-transform duration-300">
+                <RiGroupLine size={30} className="mr-1" />
+                Про нас
+              </li>
+              <li className="p-3 text-center flex items-center text-3xl font-extrabold mr-20 hover:text-red-500 hover:translate-x-5 transition-transform duration-300">
+                <GoTasklist size={30} className="mr-1" />
+                Тест
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default NavbarCourse
+export default NavbarCourse;
